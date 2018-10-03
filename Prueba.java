2ee1390bd74589e96568dc55780b31469caf6b5d
NewRandomAge <newrandomage@gmail.com>
2018-10-03 12:34:51 +0200

    Añadido lugar de nacimiento en la linea siguiente

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,6 @@
 public class Prueba{
 	public static void main(String args[]){
-		System.out.println("Alejandro Garcia Gonzalez")
+		System.out.println("Alejandro Garcia Gonzalez");
+		System.out.println("La Baneza");
 	}
 }
