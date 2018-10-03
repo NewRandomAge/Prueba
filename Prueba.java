f857f2d8a8bbe28e80459607ef490e3abf2f6d35
NewRandomAge <newrandomage@gmail.com>
2018-10-03 12:35:49 +0200

    Añadido lugar de residencia actual en la linea siguiente

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba{
 	public static void main(String args[]){
 		System.out.println("Alejandro Garcia Gonzalez");
 		System.out.println("La Baneza");
+		System.out.println("Leon");
 	}
 }
